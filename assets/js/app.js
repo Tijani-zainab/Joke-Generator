a1f0b7b371249bc9231f7c5ca5108094570473c4
Tijani-zainab <jani.tijj@gmail.com>
2021-02-12 16:40:14 +0100

    programmers joke generator, api, async and await

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+const jokeText = document.getElementById("joke-display");
+const jokeBtn = document.getElementById("jokebtn");
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke();
+
+async function generateJoke() {
+    const res = await fetch(
+        "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+    )
+    const data = await res.json();
+    console.log(data)
+    console.log(data.joke)
+    console.log(data.delivery)
+    let joke = ""
+    if (data.joke == undefined) {
+        joke = `${data.setup} <br /> ${data.delivery}`
+    } else {
+        joke = data.joke
+    }
+    jokeText.innerHTML = joke
+}
